f7cb027eb7401e09ed7aa91e8feb614b79960da9
Elliot Nicolás <A00840918@tec.mx>
2025-09-17 09:57:54 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -15,10 +15,13 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+
+current_vel = 200 #inicializar variable de vel
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
+    global current_vel
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -31,6 +34,7 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+        current_vel = max(50, current_vel - 20) #aumentar velocidad
     else:
         snake.pop(0)
 
@@ -41,15 +45,18 @@ def move():
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    #ontimer(move, v)
+    ontimer(move, current_vel)
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+velocity = 10
+onkey(lambda: change(velocity, 0), 'Right')
+onkey(lambda: change(-velocity, 0), 'Left')
+onkey(lambda: change(0, velocity), 'Up')
+onkey(lambda: change(0, -velocity), 'Down')
 move()
+
 done()
